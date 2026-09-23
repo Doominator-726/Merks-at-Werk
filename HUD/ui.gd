932064f8009fe329d43d6f0203aefbedcaa2433d
Doominator-726 <nate.r.laing@gmail.com>
2026-09-23 17:31:45 -0400

    News Ticker
    Gay frogs do be divorcing

--- HUD/ui.gd
+++ HUD/ui.gd
@@ -3,36 +3,19 @@ extends CanvasLayer
 var max_scroll_size: int = 0
 var news_scroll_speed: int = 200
 
-var ticker_messages = ["Hey hey hey", "No no no", "Yes yes yes", "Hey hey hey", "No no no", "Yes yes yes", "Hey hey hey", "No no no", "Yes yes yes"]
+@export var ticker_message: String
+var ticker_increment: int = 1
 
-func _ready() -> void:
-	populate_ticker()
-	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("Pause"):
 		get_tree().paused = !get_tree().paused
 		$Message/Label.visible = !$Message/Label.visible
 		
-	#print(max_scroll_size)
 	# Handle News Ticker
-	$"News Ticker".scroll_horizontal += news_scroll_speed * delta
-	print($"News Ticker".scroll_horizontal, " ", max_scroll_size)
-	if $"News Ticker".scroll_horizontal >= max_scroll_size:
-		$"News Ticker".scroll_horizontal = 0
-		
-func populate_ticker():
-	var blank_space = Label.new()
-	blank_space.text = "                      "
+	if ticker_increment <= len(ticker_message):
+		$"News Ticker2".text += ticker_message.substr(ticker_increment, 1)
+		ticker_increment += 1
+	else:
+		ticker_increment = 0
 	
-	$"News Ticker/HBoxContainer".add_child(blank_space)
-	
-	for message in ticker_messages:
-		var label = Label.new()
-		label.text = message
-		$"News Ticker/HBoxContainer".add_child(label)
-		
-	$"News Ticker/HBoxContainer".add_child(blank_space.duplicate())
-		
-	await get_tree().process_frame
-	max_scroll_size = $"News Ticker/HBoxContainer".size.x / 2
